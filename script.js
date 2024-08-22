88600bb0b1e1f596271c144d4f1c2fa1fc0d4006
Anisha-Arul <anishaarul605@gmail.com>
2024-08-22 21:12:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function addtodo(){
+    var todoinput = document.getElementById("todo-input");
+    var todotext = todoinput.value.trim();
+
+
+    if(todotext !== ""){
+
+         var li = document.createElement("li");
+         li.textContent = todotext;
+
+
+         
+         
+         var deletebutton = document.createElement("button");
+         deletebutton.textContent="clear";
+         deletebutton.classList.add("delete", "btn");
+         deletebutton.onclick=function(){
+            li.remove();
+
+         };
+         li.appendChild(deletebutton);
+        
+
+
+        document.getElementById("todo-list").appendChild(li);
+
+        todoinput.value = "";
+    }
+
+}
